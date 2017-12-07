b12e4dd186aa2f6bc569a6b8987f9d60ede92467
MINI-JEROME-HP\Jerome <jeromepalos@gmail.com>
2017-12-07 20:47:41 +0100

    Test for Color and Deck

---
+++ pokedeck/src/test/java/upmc/pcg/game/ColorTest.java
@@ -0,0 +1,39 @@
+package upmc.pcg.game;
+
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ColorTest {
+    
+    public ColorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void colorTest(){
+        int a =32;
+        Color color = new Color(a);
+        assertNotNull(color);
+    }
+}
---
+++ pokedeck/src/test/java/upmc/pcg/game/DeckTest.java
@@ -0,0 +1,30 @@
+package upmc.pcg.game;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+public class DeckTest extends TestCase {
+    
+    public DeckTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    public void testHello() {}
+    
+    @Test
+    public void deckTest(){
+        Deck deck = new Deck("testunnom");
+        assertNotNull(deck);
+        assertTrue("testunnom"==deck.ViewNameDeck());
+    }
+}
